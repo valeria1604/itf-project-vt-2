619aec6f12b157d5ef20ba478416a25d8a661f0b
Valeria <valtpocketbook@gmail.com>
2023-08-06 17:08:14 +0200

    TAM2 fixes

--- src/main/java/com/company/TAM2/Main.java
+++ src/main/java/com/company/TAM2/Main.java
@@ -7,6 +7,9 @@ import java.util.Map;
 public class Main{
 
     public static final int COLUMNS_COUNT = 2;
+    public static final String COMA = ",";
+    public static final int ACTION_INDEX = 0;
+    public static final int COUNT_INDEX = 1;
 
     public static void main(String[] args) {
         String fromFileName =  "orange.csv";
@@ -23,25 +26,32 @@ public class Main{
             String line;
 
             while ((line = reader.readLine()) != null){
-                String[] parts = line.split(",");
-                if(parts.length != COLUMNS_COUNT){
-                    throw new IllegalArgumentException("Not correct data");
-                }
-
-                String action = parts[0].trim();
-                int count = Integer.parseInt(parts[1].trim());
-
-                actionCount.put(action, count);
+                putAction(actionCount, line);
             }
-
-            writer.write("supply = " + actionCount.get("supply"));
-            writer.newLine();
-            writer.write("buy = " + actionCount.get("buy"));
-            writer.newLine();
-            writer.write("result = " + ( actionCount.get("supply") - actionCount.get("buy")));
+            writeToFile(writer, actionCount);
         }
         catch (Exception e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
+    }
+
+    private static void putAction(Map<String, Integer> actionCount, String line) {
+        String[] parts = line.split(COMA);
+        if(parts.length != COLUMNS_COUNT){
+            throw new IllegalArgumentException("Not correct data");
+        }
+
+        String action = parts[ACTION_INDEX].trim();
+        int count = Integer.parseInt(parts[COUNT_INDEX].trim());
+
+        int currentCount = actionCount.getOrDefault(action, 0);
+        actionCount.put(action, currentCount + count);
+    }
+
+    private static void writeToFile(BufferedWriter writer, Map<String, Integer> actionCount) throws IOException {
+        String finalReport = "supply = " + actionCount.get("supply")
+                + "\s" + "buy = " + actionCount.get("buy")
+                + "\s" + "result = " + (actionCount.get("supply") - actionCount.get("buy"));
+        writer.write(String.valueOf(finalReport));
     }
 }
