4176f025ec04a943f3022939cc910f73c7a44b39
Valeria <valtpocketbook@gmail.com>
2023-08-08 19:21:40 +0200

    TAM2 refactoring, changed gitignore

--- src/main/java/com/company/TAM2/Main.java
+++ src/main/java/com/company/TAM2/Main.java
@@ -1,10 +1,12 @@
 package com.company.TAM2;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Main{
+public class Main {
 
     public static final int COLUMNS_COUNT = 2;
     public static final String COMA = ",";
@@ -12,46 +14,58 @@ public class Main{
     public static final int COUNT_INDEX = 1;
 
     public static void main(String[] args) {
-        String fromFileName =  "orange.csv";
-        String toFileName = "result.txt";
+        String fromFileName = "src/main/resources/orange.csv";
+        String toFileName = "src/main/resources/result.txt";
         getStatistic(fromFileName, toFileName);
     }
 
     public static void getStatistic(String fromFileName, String toFileName) {
-        try (
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-        ) {
-            Map<String, Integer> actionCount = new HashMap<>();
-            String line;
 
-            while ((line = reader.readLine()) != null){
-                putAction(actionCount, line);
+        List<String> dataFromFile = readFromFile(fromFileName);
+        Map<String, Integer> actionCount = new HashMap<>();
+
+        for (String line : dataFromFile) {
+            String[] parts = line.split(COMA);
+            if (parts.length != COLUMNS_COUNT) {
+                throw new IllegalArgumentException("Not correct data");
             }
-            writeToFile(writer, actionCount);
+
+            String action = parts[ACTION_INDEX].trim();
+            int count = Integer.parseInt(parts[COUNT_INDEX].trim());
+
+            int currentCount = actionCount.getOrDefault(action, 0);
+            actionCount.put(action, currentCount + count);
         }
-        catch (Exception e) {
+
+        String finalReport = createReport(actionCount);
+        writeToFile(toFileName, finalReport);
+    }
+
+    private static String createReport(Map<String, Integer> actionCount) {
+        return "supply = " + actionCount.get("supply") + System.lineSeparator() +
+                "buy = " + actionCount.get("buy") + System.lineSeparator() +
+                "result = " + (actionCount.get("supply") - actionCount.get("buy"));
+    }
+
+    private static List<String> readFromFile(String fromFileName) {
+        List<String> resultList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                resultList.add(line);
+            }
+        } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
+        return resultList;
     }
 
-    private static void putAction(Map<String, Integer> actionCount, String line) {
-        String[] parts = line.split(COMA);
-        if(parts.length != COLUMNS_COUNT){
-            throw new IllegalArgumentException("Not correct data");
+    private static void writeToFile(String toFileName, String dataToWrite) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(dataToWrite);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
-
-        String action = parts[ACTION_INDEX].trim();
-        int count = Integer.parseInt(parts[COUNT_INDEX].trim());
-
-        int currentCount = actionCount.getOrDefault(action, 0);
-        actionCount.put(action, currentCount + count);
-    }
-
-    private static void writeToFile(BufferedWriter writer, Map<String, Integer> actionCount) throws IOException {
-        String finalReport = "supply = " + actionCount.get("supply")
-                + "\s" + "buy = " + actionCount.get("buy")
-                + "\s" + "result = " + (actionCount.get("supply") - actionCount.get("buy"));
-        writer.write(String.valueOf(finalReport));
     }
 }
